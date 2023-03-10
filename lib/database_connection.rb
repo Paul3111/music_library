88452f9a5cff7b8ff86bd187b93d3c3f18b76eff
Paul Lazar <paullazar@MASTUDENT-Paul-Lazar-C17GMAXPQ6L4.local>
2023-03-10 22:27:59 +0000

    updates

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -19,12 +19,12 @@ class DatabaseConnection
   # This method executes an SQL query 
   # on the database, providing some optional parameters
   # (you will learn a bit later about when to provide these parameters).
-  def self.exec_params(query, params)
-    if @connection.nil?
-      raise 'DatabaseConnection.exec_params: Cannot run a SQL query as the connection to'\
-      'the database was never opened. Did you make sure to call first the method '\
-      '`DatabaseConnection.connect` in your app.rb file (or in your tests spec_helper.rb)?'
-    end
-    @connection.exec_params(query, params)
-  end
+  #def self.exec_params(query, params)
+  #  if @connection.nil?
+  #    raise 'DatabaseConnection.exec_params: Cannot run a SQL query as the connection to'\
+   #   'the database was never opened. Did you make sure to call first the method '\
+  #    '`DatabaseConnection.connect` in your app.rb file (or in your tests spec_helper.rb)?'
+  #  end
+  #  @connection.exec_params(query, params)
+  #end
 end
