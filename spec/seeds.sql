75fc934041cf19cd5cf4b8a8ab3efb3c3b363825
Paul Lazar <paullazar@MASTUDENT-Paul-Lazar-C17GMAXPQ6L4.local>
2023-03-10 22:37:43 +0000

    updates_
    asd
    asd
    asd
    a
    q
    quit
    clear
    
    ;
    '
    
    '
    |

---
+++ spec/seeds.sql
@@ -0,0 +1,12 @@
+CREATE TABLE albums (
+    id SERIAL,
+    title TEXT,
+    release_year TEXT,
+    artist_id INTEGER
+);
+
+CREATE TABLE artists (
+    id SERIAL,
+    title name,
+    genre TEXT
+    );
